a6aed196b4ad7254973aeca64936ff6f2486edd1
Роман Небельський <nebelsky2012@gmail.com>
2020-12-15 01:54:01 +0200

    done

--- hw6/Knight.java
+++ hw6/Knight.java
@@ -14,23 +14,19 @@ public class Knight extends Warrior
     public void attack(Warrior warrior)
     {
         var d = Math.random();
-        if (d < 0.6)
+        if (d < 0.8)
         {
             warrior.setHp(warrior.getHp()- this.getDamage());
-            System.out.println(this.getClass().getName() + "punched " + warrior.getClass().getName()+ " with  "+ this.getDamage()  );
+            System.out.println(this.getClass().getSimpleName() + " punched " + warrior.getClass().getSimpleName()+ " with "+  this.getDamage()  );
         }
 
-        else if (d < 0.8){
+        else {
             warrior.setHp(warrior.getHp()- this.getDamage() - this.getCritDamage()); //
-            System.out.println(this.getClass().getName() + "punched " + warrior.getClass().getName()+ " with  "+ (this.getDamage() + this.getCritDamage()) );
+            System.out.println(this.getClass().getSimpleName() + " punched " + warrior.getClass().getSimpleName()+ " with "+ (this.getDamage() + this.getCritDamage()) );
 
         }
 
-        else{
-            warrior.setHp(warrior.getHp()- this.getDamage());
-            System.out.println(this.getClass().getName() + "punched " + warrior.getClass().getName()+ " with  "+ this.getDamage()  );
-        }
-        System.out.println("Warrior " +warrior.getClass().getName() + "has "  + warrior.getHp()+ "hp");
+        System.out.println("Warrior " +warrior.getClass().getSimpleName() + " has "  + warrior.getHp()+ " hp");
 
     }
 }
--- hw6/Main.java
+++ hw6/Main.java
@@ -2,10 +2,10 @@ package hw6;
 import java.util.Scanner;
 public class Main {
     //You can change these values//
-    public static Samurai samurai = new Samurai(100, 44, 40);
-    public static Archer archer = new Archer(80, 44, 40);
-    public static Knight knight = new Knight(150, 44, 40);
-    public static Kamikaze kamikaze = new Kamikaze(50, 44, 40);
+    public static Samurai samurai = new Samurai(100, 36, 40);
+    public static Archer archer = new Archer(90, 20, 40);
+    public static Knight knight = new Knight(150, 25, 40);
+    public static Kamikaze kamikaze = new Kamikaze(70, 40, 40);
 
 
     public static void main(String[] args) {
@@ -17,41 +17,72 @@ public class Main {
 
         fillArr(blue);
         fillArr(red);
-        while (true) {
-            for (int i = 0; i < blue.length; i++) {
 
+        while (checkTeamIsLive(blue) && checkTeamIsLive(red))
+        {
+
+            for (int i = 0; i < blue.length; i++) {
                 int a = 0;
-                int b = blue.length - 1;
+                int b = blue.length;
                 int random_number1 = a + (int) (Math.random() * b);
                 int random_number2 = a + (int) (Math.random() * b);
-                if (blue[random_number1].isLive() && red[random_number2].isLive()) {
 
-                    blue[random_number1].attack(red[random_number2]);
-                }
                 random_number1 = a + (int) (Math.random() * b);
                 random_number2 = a + (int) (Math.random() * b);
 
-                if (red[random_number1].isLive() && blue[random_number2].isLive()) {
-                    red[random_number1].attack(blue[random_number2]);
+                if ((a + (int) (Math.random() * b)) % 2 == 0) {
+                    if (red[random_number1].isLive() && blue[random_number2].isLive()) {
+                        red[random_number1].attack(blue[random_number2]);
+                    }
+                    random_number1 = a + (int) (Math.random() * b);
+                    random_number2 = a + (int) (Math.random() * b);
+                    if (blue[random_number1].isLive() && red[random_number2].isLive()) {
+                        blue[random_number1].attack(red[random_number2]);
+                    }
+                }
+                else {
+                    if (blue[random_number1].isLive() && red[random_number2].isLive()) {
+                        blue[random_number1].attack(red[random_number2]);
+                    }
+                    random_number1 = a + (int) (Math.random() * b);
+                    random_number2 = a + (int) (Math.random() * b);
+                    if (red[random_number1].isLive() && blue[random_number2].isLive()) {
+                        red[random_number1].attack(blue[random_number2]);
+                    }
+
                 }
             }
-            if (!checkTeamIsLive(blue) || !checkTeamIsLive(red))
-                break;
 
         }
-        if (checkTeamIsLive(blue))
+        if(checkTeamIsLive(blue))
         {
             System.out.println("Blue won");
         }
-        else {
+        else if(checkTeamIsLive(red))
+        {
             System.out.println("Red won");
         }
+        else
+        {
+            System.out.println("Draw!");
+        }
+
+        for(int i = 0;i< blue.length;i++)
+        {
+            System.out.println(blue[i].isLive() + " - blue isLive");
+        }
+        for(int i = 0;i< blue.length;i++)
+        {
+            System.out.println(red[i].isLive() + " - red isLive");
+        }
+
 
     }
         /*for(int i = 0;i<blue.length;i++)
         {
             System.out.println(blue[i].getClass().getName());
         }*/
+
 
 
     public static void fillArr(Warrior[] team )
@@ -77,19 +108,28 @@ public class Main {
         arrLength++;
     }while(arrLength< team.length);
 
+
     }
     public static boolean checkTeamIsLive(Warrior[] team)
     {
-        boolean check = false;
-        for(int i = 0;i< team.length;i++)
+       int check = 0;
+        for(int i = 0; i< team.length;i++)
         {
-            if(!team[i].isLive())
-                check = check || team[i].isLive();
-
-            else
-                check= true;
+            if(team[i].isLive())
+            {
+                ++check;
+            }
 
         }
-        return check;
+        if(check!= 0){
+            return true;
+        }
+        else{
+
+            return false;
+        }
+
     }
+
+    public static void teamFight(double randomNum1 , double randomNum2){}
 }
